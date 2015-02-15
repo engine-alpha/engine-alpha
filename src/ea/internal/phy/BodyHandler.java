e4346739d6e4dc741513d267250d73254092c9ac
Michael Andonie <michael@andonie.net>
2015-02-16 00:54:37 +0100

    Added: BodyHandler class stub (w/ minor functionality) Extended: Physik class - preparation for basic Raum integration is done

---
+++ src/ea/internal/phy/BodyHandler.java
@@ -0,0 +1,83 @@
+package ea.internal.phy;
+
+import ea.Raum;
+import ea.internal.ano.NoExternalUse;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Ein <code>Body-Handler</code> kümmert sich um die <i>physikalische Darstellung</i> eines <code>Raum</code>-Objekts.<br />
+ * Er übernimmt zwei wesentliche Aufgaben:
+ * <ul>
+ *     <li>Die Kontrolle und Steuerung innerhalb der <b>Physik-Engine</b> aus Sicht des respektiven Raum Objekts.</li>
+ *     <li>Die Speicherung der <i>räumlichen Eigenschaften</i> (Position und Rotation) des respektiven Raum-Objekts.</li>
+ * </ul>
+ * Created by andonie on 15.02.15.
+ */
+public class BodyHandler {
+
+    /**
+     * Das Raum-Objekt, um das sich dieses <code>BodyHandler</code>-Objekt kümmert.
+     */
+    private final Raum raum;
+
+    /**
+     * Die Physik des Handlers. Ist zu Beginn undefiniert. Kann nur einmal gesetzt werden.
+     */
+    private Physik physik;
+
+    /**
+     * Die Liste mit Physik-Ausführungen, die vor der Anmeldung einer Physik geschehen sind. Werden nachgeholt, sobald
+     * eine Physik angemeldet wurde.
+     */
+    private List<PrePhysicsInvocation> prePhysicInvocations = new LinkedList<PrePhysicsInvocation>();
+
+    /**
+     * Erstellt einen neuen Body-Handler
+     * @param raum  
+     */
+    @NoExternalUse
+    public BodyHandler(Raum raum) {
+        this.raum = raum;
+    }
+
+    /**
+     * Gibt an, ob dieser Handler bereits eine Physik-Umgebung hat.
+     * @return  Die
+     */
+    @NoExternalUse
+    public boolean hatPhysik() {
+        return physik!=null;
+    }
+
+    /**
+     * Setzt die Physik für diesen Handler.
+     * Diese Methode kann nur einmal aufgerufen werden. Wird diese Methode ein zweites mal mit einem
+     * anderen Physik-Objekt aufgerufen, so wird eine Exception geworfen.
+     * @param physik    die zu setzende Physik.
+     */
+    @NoExternalUse
+    public void physikSetzen(Physik physik) {
+        if(hatPhysik()) {
+            if (physik == this.physik) return;
+            throw new IllegalStateException("Ein Raum-Objekt, das bereits auf einer Zeichenebene war, wurde an einer" +
+                    " neuen Zeichenebene eingefügt. Das ist nicht möglich.");
+        }
+        this.physik = physik;
+        for(PrePhysicsInvocation ppi : prePhysicInvocations) {
+            ppi.physicsCatchup();
+        }
+        prePhysicInvocations = null; //Speicher wieder freigeben (nach Garbage Collection)
+    }
+
+    /**
+     * Dieses Interface beschreibt eine Physik-relevante Aktion, die noch nicht durchführbar ist, da
+     * die aktive Physik noch nicht bekannt ist. Die Aktion wird gespeichert und beim setzen der Physik geändert.
+     */
+    @NoExternalUse
+    private interface PrePhysicsInvocation {
+        public abstract void physicsCatchup();
+    }
+}
--- src/ea/internal/phy/Physik.java
+++ src/ea/internal/phy/Physik.java
@@ -1,7 +1,11 @@
 package ea.internal.phy;
 
+import ea.*;
+
+import ea.internal.ano.NoExternalUse;
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.World;
+import sun.misc.Version;
 
 /**
  * Die Physik-Klasse ist die (nicht objektgebundene) Middleware zwischen der JBox2D Engine und der EA.
@@ -19,6 +23,37 @@ public class Physik {
      */
     private final World world;
 
+    /**
+     * Umrechnungsgröße zwischen Größen der Physik-Engine und der Zeichenebene der EA.
+     * Gibt an, wie viele Pixel genau einen Meter ausmachen.<br/>
+     *
+     * <b>Einheit: [px/m]</b>
+     */
+    private float pixelProMeter = 30f;
+
+    /**
+     * Gibt die Umrechnungsgröße zwischen Größen der Physik-Engine und der Zeichenebene der EA an.
+     * @return  Gibt an, wie viele Pixel genau einen Meter ausmachen.<br />
+     *          <b>Einheit: [px/m]</b>
+     */
+    public float getPixelProMeter() {
+        return pixelProMeter;
+    }
+
+    /**
+     * Setzt die Umrechnungsgröße zwischen Größen der Physik-Engine und der Zeichenebene der EA.
+     * @param pixelProMeter Die Anzahl an Pixeln, die genau einen Meter ausmachen.<br />
+     *                      <i>Die Größe ist unabhängig vom Kamerazoom.</i>< br/>
+     *                      <b>Einheit: [px/m]</b>
+     */
+    public void setPixelProMeter(float pixelProMeter) {
+        this.pixelProMeter = pixelProMeter;
+    }
+
+    /**
+     * Erstellt eine neue standardisierte Physik (Schwerkraft senkrecht nach unten, 9,81 m/s^2)
+     */
+    @NoExternalUse
     public Physik() {
         this.world = new World(new Vec2(0f, -9.81f)); //Erstelle standard-World mit Standard-Gravitation.
     }
@@ -27,7 +62,32 @@ public class Physik {
      * Gibt den World-Parameter der Physik aus.
      * @return  Der JB2D-World-Parameter der Welt.
      */
+    @NoExternalUse
     public World getWorld() {
         return world;
+    }
+
+    /**
+     * Übersetzt einen EA-Vektor in einen JB2D-Vektor auf Basis des gesetzten Pixel/Meter-Verhältnisses.
+     * @param eaV   Ein EA-Vektor.
+     * @return      Der analoge Vektor in der JB2D-Engine.
+     */
+    @NoExternalUse
+    public Vec2 fromVektor(Vektor eaV) {
+        float x = eaV.x / pixelProMeter;
+        float y = eaV.y / pixelProMeter;
+        return new Vec2(x,y);
+    }
+
+    /**
+     * Übersetzt einen JB2D-Vektor in einen EA-Vektor auf Basis des gesetzten Pixel/Meter-Verhältnisses.
+     * @param jb2dV Ein JB2D-Vektor.
+     * @return      Der analoge Vektor im EA-Format auf der Zeichenebene.
+     */
+    @NoExternalUse
+    public Vektor fromVec2(Vec2 jb2dV) {
+        float x = jb2dV.x * pixelProMeter;
+        float y = jb2dV.y * pixelProMeter;
+        return new Vektor(x,y);
     }
 }
