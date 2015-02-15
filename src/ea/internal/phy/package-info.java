8744111ff0dc17a22978b3a392e56501b67bf4fc
Michael Andonie <michael@andonie.net>
2015-02-15 20:27:43 +0100

    Added package info for physics package (ea.internal.phy) with extensive copyright / license info

---
+++ src/ea/internal/phy/package-info.java
@@ -0,0 +1,33 @@
+/**
+ * Dieses Paket enthält die Klassen, die innerhalb der Engine als Middleware zwischen der externen Bibliothek
+ * <i>JBox2D</i> von Daniel Murhpy und der Engine-API fungieren.
+ *
+ * Das Projekt <a href="https://github.com/jbox2d/jbox2d">JBox2D-Projekt</a> ist unter der
+ * <i>open source zlib license</i> veröffentlicht. Dies ist die
+ *
+ * <blockquote>
+ *
+ *       Copyright (c) 2014, Daniel Murphy
+         All rights reserved.
+
+        <ul>
+            <li>Redistributions of source code must retain the above copyright notice, this
+                 list of conditions and the following disclaimer.</li>
+            <li>Redistributions in binary form must reproduce the above copyright notice, this
+                 list of conditions and the following disclaimer in the documentation and/or
+                 other materials provided with the distribution.</li>
+        </ul>
+ *       THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+         ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+         WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+         DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+         ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+         (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+         LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+         ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+         (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+         SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * </blockquote>
+ * Created by andonie on 15.02.15.
+ */
+package ea.internal.phy;
