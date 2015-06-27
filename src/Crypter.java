24c31f0b8ca1df9792675deaee7d55ee170c18d4
Craumix <soeren@guertler.com>
2015-06-27 20:48:21 +0200

    Create Crypter.java

---
+++ src/Crypter.java
@@ -0,0 +1,46 @@
+package ea;
+
+import java.nio.charset.Charset;
+import java.security.Key;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+import sun.misc.*;
+
+public class Crypter {
+	 public String verschluesseln(String text, String schluessel){
+	 if(schluessel.lenght() == 16){
+	   Key key;
+	   String encryptedValue=null;
+		try {
+	  	key = new SecretKeySpec(schluessel.getBytes(Charset.forName("UTF-8")), "AES");
+	  	Cipher c = Cipher.getInstance(ALGORITHM);
+	  	c.init(Cipher.ENCRYPT_MODE, key);
+	  	byte[] encValue = c.doFinal(valueToEnc.getBytes());
+	  	encryptedValue = new BASE64Encoder().encode(encValue);
+		}catch(Exception e){e.printStackTrace();}
+	    return encryptedValue;
+	    }else{
+	    System.out.println("Der SChluessel muss 16 Zeichen haben!");
+	    }
+	}
+
+	public String entschluesseln(String text,String schluessel){
+	if(schluessel.lenght() == 16){
+	    Key key;
+	    String decryptedValue = null;
+		try {
+	  	key = new SecretKeySpec(schluessel.getBytes(Charset.forName("UTF-8")), "AES");
+	  	Cipher c = Cipher.getInstance(ALGORITHM);
+	  	c.init(Cipher.DECRYPT_MODE, key);
+	    byte[] decordedValue = new BASE64Decoder().decodeBuffer(encryptedValue);
+		  byte[] decValue = c.doFinal(decordedValue);
+		  decryptedValue = new String(decValue);
+		}catch(Exception e){e.printStackTrace();}
+	    return decryptedValue;
+	     }else{
+	    System.out.println("Der SChluessel muss 16 Zeichen haben!");
+	    }
+	}
+}
