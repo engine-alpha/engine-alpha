f9d9c63f6568dd6359186ef22688310da104ef12
Michael Andonie <michael@andonie.net>
2018-05-28 20:07:01 +0200

    Added: Changeable Intervals in PeriodicTask

--- src/ea/PeriodicTask.java
+++ src/ea/PeriodicTask.java
@@ -19,6 +19,8 @@
 
 package ea;
 
+import ea.internal.ano.API;
+import ea.internal.ano.NoExternalUse;
 import ea.internal.frame.Dispatchable;
 
 /**
@@ -30,7 +32,7 @@ public abstract class PeriodicTask implements Dispatchable, FrameUpdateListener 
     /**
      * Intervall in Millisekunden.
      */
-    private final int interval;
+    private int interval;
 
     /**
      * Aktuelle Zeit bis zur nächsten Ausführung.
@@ -43,14 +45,38 @@ public abstract class PeriodicTask implements Dispatchable, FrameUpdateListener 
      * @param interval Zeit zwischen den Ausführungen in Millisekunden.
      */
     public PeriodicTask(int interval) {
-        this.interval = interval;
+        setInterval(interval);
         this.countdown = interval;
+    }
+
+    /**
+     * Setzt das intervall dieses Periodischen Tasks neu.
+     * @param interval  Das neue Intervall. Zeit zwischen den Ausführungen in Millisekunden.
+     *                  Muss größer als 0 sein.
+     */
+    @API
+    public void setInterval(int interval) {
+        if(interval <= 0) {
+            throw new RuntimeException("Das Interval eines Periodischen Tasks muss größer als 0 sein. Eingabe war "
+             + interval + ".");
+        }
+        this.interval = interval;
+    }
+
+    /**
+     * Gibt das aktuelle Intervall des periodischen Tasks aus.
+     * @return  Das aktuelle Intervall. Zeit zwischen den Ausführungen in Millisekunden.
+     */
+    @API
+    public int getInterval() {
+        return interval;
     }
 
     /**
      * @param frameDuration Die Zeit in Millisekunden, die seit dem letzten Update vergangen
      */
     @Override
+    @NoExternalUse
     public void onFrameUpdate(int frameDuration) {
         this.countdown -= frameDuration;
 
