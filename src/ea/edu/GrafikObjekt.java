95c300b96b8cf06a1f334dc4c042c5ded3476def
Michael Andonie <michael@andonie.net>
2018-08-05 00:33:11 +0200

    Added documentation for GrafikObjekt

--- src/ea/edu/GrafikObjekt.java
+++ src/ea/edu/GrafikObjekt.java
@@ -1,9 +1,16 @@
 package ea.edu;
 
 /**
- * Das Interface, aus dem sich alle
+ * Das Interface, das sämtliche grafischen EDU-Enduser-Klassen implementieren. Dies ermöglicht
+ * Interoperabilität wesentlicher Funktionen mit eigentlichen <code>Actor</code>-Parametern
+ * (Kollision-Checks, Abstände, etc.) ohne dass der für den Schüler erkennbare Funktionsumfang zu
+ * unübersichtlich wird.
  */
 public interface GrafikObjekt {
 
+    /**
+     * Gibt einen EDU-Actor zurück.
+     * @return  Der EDU-Actor, der gerade vom entsprechenden EDU-Objekt gewrappt wird.
+     */
     EduActor getActor();
 }
