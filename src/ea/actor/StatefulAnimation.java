c3da067df13ff413afd8bb4625cca39561d1a266
Michael Andonie <michael@andonie.net>
2019-08-07 17:30:06 +0200

    Removed testing Stuff

--- src/ea/actor/StatefulAnimation.java
+++ src/ea/actor/StatefulAnimation.java
@@ -249,8 +249,6 @@ public class StatefulAnimation extends Actor {
             return; // we don't have a state yet
         }
 
-        System.out.println("Internal Frame Update " + frameDuration + " frameDur, " + currentTime + " currentTime");
-
         currentTime += frameDuration;
 
         AnimationFrame currentFrame = currentAnimation[currentIndex];
