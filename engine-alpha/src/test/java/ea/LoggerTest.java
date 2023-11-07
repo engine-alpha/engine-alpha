6b71217bcc68ffee4db0de55680f713c533b3c2c
Josef Friedrich <josef@friedrich.rocks>
2023-11-07 09:25:36 +0100

    Unify white space handling
    Most classes are written with a 4-space indentation and do not use tabs

--- engine-alpha/src/test/java/ea/LoggerTest.java
+++ engine-alpha/src/test/java/ea/LoggerTest.java
@@ -9,9 +9,9 @@ import java.nio.file.Paths;
 import static org.junit.Assert.assertTrue;
 
 public class LoggerTest {
-	@Test
-	public void fileExists () {
-		Logger.error("LoggerTest", "lorem ipsum");
-		assertTrue(Files.exists(Paths.get("engine-alpha.log")));
-	}
+    @Test
+    public void fileExists () {
+        Logger.error("LoggerTest", "lorem ipsum");
+        assertTrue(Files.exists(Paths.get("engine-alpha.log")));
+    }
 }
