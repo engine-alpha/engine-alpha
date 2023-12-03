9c71141788e0fbb781564d9a5f86abbeea48320c
Josef Friedrich <josef@friedrich.rocks>
2023-12-03 17:15:50 +0100

    Capture stderr and test stderr (#45)
    This commit removes the misleading error message when running the tests in the terminal using mvn test.

--- engine-alpha/src/test/java/ea/LoggerTest.java
+++ engine-alpha/src/test/java/ea/LoggerTest.java
@@ -1,17 +1,37 @@
 package ea;
 
-import ea.internal.util.Logger;
-import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ea.internal.util.Logger;
 
 public class LoggerTest {
+
+    private final PrintStream standardErr = System.err;
+    private final ByteArrayOutputStream errStreamCaptor = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() {
+        System.setErr(new PrintStream(errStreamCaptor));
+    }
+
+    @After
+    public void tearDown() {
+        System.setErr(standardErr);
+    }
+
     @Test
-    public void fileExists () {
+    public void fileExists() {
         Logger.error("LoggerTest", "lorem ipsum");
+        assertTrue(errStreamCaptor.toString().indexOf("lorem ipsum") > -1);
         assertTrue(Files.exists(Paths.get("engine-alpha.log")));
     }
 }
